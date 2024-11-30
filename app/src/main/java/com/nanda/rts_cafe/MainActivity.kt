2a5b57fe55a14cc5efd1da1471c49f3c0afbe132
Rasyaa21 <naufalrasya21907@gmail.com>
2024-11-30 07:34:13 +0700

    adsadsa

--- app/src/main/java/com/nanda/rts_cafe/MainActivity.kt
+++ app/src/main/java/com/nanda/rts_cafe/MainActivity.kt
@@ -5,15 +5,25 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appname.Adapter
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nanda.rts_cafe.Fragments.CartFragment
+import com.nanda.rts_cafe.Fragments.ProfileFragment
+import com.nanda.rts_cafe.Fragments.SearchFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val SearchFragment = SearchFragment()
+        val ProfileFragment = ProfileFragment()
+        val CartFragment = CartFragment()
+
 
         val menuItems = listOf(
             Adapter.MenuItem(R.drawable.foto, "Spaghetti Bolognese", "32.000 IDR", "5.2"),
@@ -32,5 +42,41 @@ class MainActivity : AppCompatActivity() {
                 outRect.set(spacing, spacing, spacing, spacing)
             }
         })
+
+        toggleHomeVisibility(true)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    toggleHomeVisibility(true)
+                    supportFragmentManager.popBackStack(null, 0) // Clear back stack
+                    true
+                }
+                else -> {
+                    toggleHomeVisibility(false)
+                    val selectedFragment = when (menuItem.itemId) {
+                        R.id.search -> SearchFragment()
+                        R.id.cart -> CartFragment()
+                        R.id.profile -> ProfileFragment()
+                        else -> null
+                    }
+                    selectedFragment?.let { loadFragment(it) }
+                    true
+                }
+            }
+        }
     }
-}
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+        private fun toggleHomeVisibility(showHome: Boolean) {
+            findViewById<View>(R.id.main).visibility = if (showHome) View.VISIBLE else View.GONE
+            findViewById<View>(R.id.fragment_container).visibility = if (showHome) View.GONE else View.VISIBLE
+        }
+    }
